246cbded7739de08fa68eff8aac472bd2380bb15
Nathaniel Mosco <natemosco@gmail.com>
2019-09-16 15:51:34 -0400

    3 Routes with paths / ; /about ; and /contact created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path="/"></Route>
+    <Route path="/about"></Route>
+    <Route path="/contact"></Route>
   </div>
 );
 
